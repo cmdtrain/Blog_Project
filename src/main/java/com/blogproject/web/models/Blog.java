e7791d2b55488d136b533f1cc887dc2dd4476bc0
cmdtrain <72573030+cmdtrain@users.noreply.github.com>
2024-09-05 16:42:37 +0300

    added files to repository

---
+++ src/main/java/com/blogproject/web/models/Blog.java
@@ -0,0 +1,38 @@
+package com.blogproject.web.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "blogs")
+
+public class Blog {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private Long id;
+	private String title;
+	private String photoUrl;
+	private String content;
+	@CreationTimestamp
+	private LocalDateTime createdOn;
+	@UpdateTimestamp
+	private LocalDateTime updatedOn;
+}
